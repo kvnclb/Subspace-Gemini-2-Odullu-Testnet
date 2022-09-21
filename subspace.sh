77ed238da99369a940b29a2adf1b70fd403548f0
Lexar <burakinkaya@sabanciuniv.edu>
2022-09-22 01:04:07 +0300

    Update subspace.sh

--- subspace.sh
+++ subspace.sh
@@ -14,7 +14,9 @@ echo -e "\e[1m\e[32m Yukleniyor... \e[0m" && sleep 2
 sudo apt update && sudo apt install ocl-icd-opencl-dev ocl-icd-libopencl1 libopencl-clang-dev libgomp1 -y && sudo apt install wget -y && cd $HOME
 
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "---------------------------------------------------------------"
+
 
 
 echo -e "\e[1m\e[91m Daha oncesinde subspace node'u calistirdiysaniz y'ye basin. Calistirmadiysaniz n'ye basin. (y/n) \e[0m"
@@ -27,7 +29,8 @@ if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
 fi
 
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "---------------------------------------------------------------"
 
 wget https://github.com/subspace/subspace/releases/download/gemini-2a-2022-sep-10/subspace-node-ubuntu-x86_64-gemini-2a-2022-sep-10
 
@@ -40,22 +43,26 @@ sudo mv subspace-farmer-ubuntu-x86_64-gemini-2a-2022-sep-10 /usr/local/bin/subsp
 sudo chmod +x /usr/local/bin/subspace*
 
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "---------------------------------------------------------------"
 
 echo -e "\e[1m\e[32m Subspace cuzdan adresinizi girin. \e[0m"
 read -p "Address: " ADDRESS
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "---------------------------------------------------------------"
 
 echo -e "\e[1m\e[32m Node isminizi girin. \e[0m"
 read -p "Node Name : " NODE_NAME
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "---------------------------------------------------------------"
 
 echo -e "\e[1m\e[32m Plot size degerini girin. (Max 100GB) \e[0m"
 read -p "Plot Size : " PLOTSIZE
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "---------------------------------------------------------------"
 
 
 echo -e "\e[1m\e[92m Node Ismi: \e[0m" $NODE_NAME
@@ -68,7 +75,8 @@ echo -e "\e[1m\e[91m    Bilgileri onayliyor musunuz? (y/n) \e[0m"
 read -p "(y/n)?" response
 if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "---------------------------------------------------------------"
 
 echo -e "\e[1m\e[32m Node icin servis dosyasi olusturuluyor... \e[0m"
 
@@ -85,7 +93,8 @@ WantedBy=multi-user.target" > $HOME/subspace-node.service
 
 sudo mv $HOME/subspace-node.service /etc/systemd/system
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "---------------------------------------------------------------"
 
 echo -e "\e[1m\e[32m Farmer icin servis dosyasi olusturuluyor \e[0m"
 
@@ -102,7 +111,8 @@ WantedBy=multi-user.target" > $HOME/subspace-farmer.service
 
 sudo mv $HOME/subspace-farmer.service /etc/systemd/system
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "---------------------------------------------------------------"
 
     sudo systemctl daemon-reload
     sudo systemctl enable subspace-farmer.service
@@ -114,7 +124,8 @@ echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo -e "\e[1m\e[32mNode baslatildi \e[0m"
 echo -e "\e[1m\e[32mFarmer baslatildi \e[0m"
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "---------------------------------------------------------------"
 
 echo -e "\e[1m\e[32mGerekli komutlar\e[0m"
 
